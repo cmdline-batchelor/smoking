f23d6ccb3fb63d0e1c6f16c9ba6aa646da443a59
cmdline-batchelor <lareab@hotmail.co.uk>
2020-12-11 17:35:00 +0000

    Update Smoke.py

--- Smoke.py
+++ Smoke.py
@@ -1,41 +1,53 @@
-wed = {
-    '15:30': 1,
-    '16:30': 1,
-    '16:50': 2,
-    '17:30': 1,
-    '18:30': 1,
-    '20:20': 1,
-    '21:30': 1,
-    '22:30': 1,
-    '25:00': 5
-}
+days = [
+    { "day_name": "wed",
+      "smoked_at": {
+        '15:30': 1,
+        '16:30': 1,
+        '16:50': 2,
+        '17:30': 1,
+        '18:30': 1,
+        '20:20': 1,
+        '21:30': 1,
+        '22:30': 1,
+        '25:00': 5
+        }
+    },
+    { "day_name": "thurs",
+        "smoked_at": {
+        '08:15': 1,
+        '08:40': 1,
+        '09:20': 1,
+        '10:00': 1,
+        '11:20': 1,
+        '11:38': 1, 
+        '12:10': 1,
+        '13:00': 1,
+        '14:26': 1,
+        '15:40': 1, 
+        '17:08': 1,
+        '18:10': 1,
+        '19:30': 1,
+        '20:20': 1,
+        '22:00': 1,
+        '23:00': 1,
+        '25:00': 2
+        }
+    }
+]
 
-thurs = {
-    '08:15': 1, 
-    '08:40': 1, 
-    '09:20': 1
-}
-
-wed_vals = wed.values()
-thurs_vals = thurs.values()
-
-def add(lst):
-    total = 0 
-    for i in lst:
-        total += i
-    return total 
-#----------------------------
-z = lambda x, y : print(x, y)
-
-#cant use for loop in this way to do 
-# what the 2 functions below do
-# doesn't work 
-
-days = ['wed', 'thurs']
+smoke_ray = []
 
 for i in days:
-    z("i:", add("i"_vals))
+    print(i["day_name"])
+    smokes = i["smoked_at"].values()
+    smokes_day = sum(smokes)
+    print(smokes_day)
+    smoke_ray.append(smokes_day)
 
-# works 
-#z('wed:', add(wed_vals))
-#z('thurs:', add(thurs_vals))
+def all_smokes():
+    total = 0 
+    for i in smoke_ray: 
+        total += i 
+    print(total)
+	
+all_smokes()
