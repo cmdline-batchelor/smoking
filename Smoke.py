d35243b3ed541deea54f76c2803107c5d3ee2054
cmdline-batchelor <lareab@hotmail.co.uk>
2021-03-08 16:46:04 +0000

    Update Smoke.py
    G

--- Smoke.py
+++ Smoke.py
@@ -8,29 +8,17 @@ days = [
         '18:30': 1,
         '20:20': 1,
         '21:30': 1,
-        '22:30': 1,
-        '25:00': 5
+        '22:30': 1
         }
     },
-    { "day_name": "thurs",
+    { "day_name":"thurs",
         "smoked_at": {
         '08:15': 1,
         '08:40': 1,
         '09:20': 1,
         '10:00': 1,
         '11:20': 1,
-        '11:38': 1, 
-        '12:10': 1,
-        '13:00': 1,
-        '14:26': 1,
-        '15:40': 1, 
-        '17:08': 1,
-        '18:10': 1,
-        '19:30': 1,
-        '20:20': 1,
-        '22:00': 1,
-        '23:00': 1,
-        '25:00': 2
+        '11:38': 1
         }
     }
 ]
@@ -38,16 +26,17 @@ days = [
 smoke_ray = []
 
 for i in days:
-    print(i["day_name"])
+    days = i["day_name"]
     smokes = i["smoked_at"].values()
-    smokes_day = sum(smokes)
-    print(smokes_day)
-    smoke_ray.append(smokes_day)
+    smokes_per_day = sum(smokes)
+    print(f"{days}: {smokes_per_day}")
+    smoke_ray.append(smokes_per_day)
 
 def all_smokes():
     total = 0 
     for i in smoke_ray: 
         total += i 
-    print(total)
+    nl = '\n'
+    print(f"{nl}{'Total:'} {total}")
 	
 all_smokes()
